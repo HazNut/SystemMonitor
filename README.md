80976ce1322616ac848344e096cb577c41a27cc5
HazNut <josephhazell@gmail.com>
2020-08-30 15:09:52 +0100

    Rename project and change goals

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# System Monitor
-System Monitor allows you to view various info about your system such as CPU usage.
+# TaskMaster
+TaskMaster allows you to view various info about your system such as CPU usage, as well as manage currently running tasks.
 
 The current version only allows you to see CPU usage, but more features will be added soon.
--- taskmaster.py
+++ taskmaster.py
@@ -5,9 +5,9 @@ import psutil
 
 
 # Callback function to update the CPU usage. Calls after() again so that it is repeatedly updated.
-def update_cpu_usage(cpu_usage, window):
-    cpu_usage.set("CPU usage: " + str(psutil.cpu_percent()))
-    window.after(1000, update_cpu_usage, cpu_usage, window)
+def update_vars(values, window):
+    values[0].set("CPU usage: " + str(psutil.cpu_percent()))
+    window.after(1000, update_vars, values, window)
 
 
 def main():
@@ -16,15 +16,18 @@ def main():
     window.title("System Monitor")
     window.minsize(300, 300)
 
+    values = []
+
     # The CPU usage is stored in a StringVar, so when it is updated, the label using it is also updated.
     cpu_usage = tk.StringVar()
     cpu_usage.set("CPU usage: " + str(psutil.cpu_percent()))
-    label = tk.Label(textvariable=cpu_usage)
-    label.pack(anchor=tk.W)
+    cpu_usage_label = tk.Label(textvariable=cpu_usage)
+    cpu_usage_label.pack(anchor=tk.W)
+    values.append(cpu_usage)
 
     # The CPU usage is updated after a second has elapsed. The update function calls it again, so it
     # repeatedly updates.
-    window.after(1000, update_cpu_usage, cpu_usage, window)
+    window.after(1000, update_vars, values, window)
 
     window.mainloop()
 
